56131b856591fe55349fd6234a2fe94662a03651
nagolove <dekar.own@gmail.com>
2025-01-10 01:02:29 +0300

    Прототип для превращения в библиотеку.

---
+++ worms_effect.h
@@ -0,0 +1,14 @@
+
+typedef struct WormsEffectOpts {
+    float scale;
+    int iteration_count;
+    int glow_size;
+    swap_direction_rarity 
+    blur_iteration_count 
+    recolor_rarity 
+    min_value 
+    max_value 
+    glow_min_Value 
+    glow_max_Value 
+    Color fade_value;
+} WormsEffectOpts;
